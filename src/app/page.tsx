8a271a329281acb44de03313b4827faee2f924c1
Osmar Petry <osmarpetry@gmail.com>
2026-09-06 20:10:51 +0200

    fix: wrap ScanWorkbench in Suspense for useSearchParams prerender

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Suspense } from "react";
+
 import { ScanWorkbench } from "@/components/scan/scan-workbench";
 
 export default function Home() {
-  return <ScanWorkbench />;
+  return (
+    <Suspense>
+      <ScanWorkbench />
+    </Suspense>
+  );
 }
